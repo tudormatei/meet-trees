665d3981e95c85ecdca44bb65006bfd4b7b999f7
Tudor Matei <66519282+tudormatei@users.noreply.github.com>
2021-11-05 20:54:16 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ from flask import Flask, request, jsonify
 import numpy as np
 import os
 from PIL import Image
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+
+db = SQLAlchemy(app)
+
+# class users(db.Model):
+#     _id = db.Column("id", db.Integer, primary_key=True)
+#     name = db.Column("name", db.String(15))
+#     name = db.Column("password", db.String(100))
+
+#     def __init__(self, name, password):
+
 
 
 def check_code(content):
@@ -18,12 +30,24 @@ def index():
     return '<h1>FLASK APP IS RUNNING!</h1>'
 
 
-@app.route('/api/result', methods=['POST'])
+@app.route('/api/register', methods=['POST'])
 def result():
     content = request.json
+    user = content['user']
+    password = content['password']
 
-    return jsonify(check_code(content))
+    return f'REGISTER: username is {user}, password is {password}'
+
+
+@app.route('/api/login', methods=['POST'])
+def result():
+    content = request.json
+    user = content['user']
+    password = content['password']
+
+    return f'LOGIN: username is {user}, password is {password}'
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
