715d73273afbdde3caf02a55c6a098cfdd85ad4a
Marc Spataru <marc.spataru@gmail.com>
2021-11-05 22:24:15 +0000

    uhhh

--- pages/creeazaEveniment.js
+++ pages/creeazaEveniment.js
@@ -3,9 +3,12 @@ export default function creeazaEveniment() {
     e.preventDefault();
     const numeeveniment = e.target.numeeveniment.value;
     const locatieeveniment = e.target.locatieeveniment.value;
+    const dataeveniment = e.target.dataeveniment.value;
+    console.log(dataeveniment);
     const eveniment = {
       name: numeeveniment,
       location: locatieeveniment,
+      date: dataeveniment,
     };
     const res = await fetch("http://localhost:5000/api/create", {
       method: "POST",
@@ -21,6 +24,8 @@ export default function creeazaEveniment() {
       <input type="text" name="numeeveniment" id="nume-evenimet"></input>
       <label htmlFor="locatie-eveniment">Locatie eveniment</label>
       <input type="text" name="locatieeveniment" id="locatie-eveniment"></input>
+      <label htmlFor="data-eveniment">Data</label>
+      <input type="date" name="dataeveniment" id="data-eveniment"></input>
       <input type="submit"></input>
     </form>
   );
