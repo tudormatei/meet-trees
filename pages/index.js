6de72dbe4b8c8bad97b8556c0c9c63e5acc8303a
Marc Spataru <marc.spataru@gmail.com>
2021-11-05 22:51:47 +0000

    lista evenimente trecute si viitoare

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,44 @@
 export default function Events(events) {
-  const listItems = events.events.map((event, index) => {
-    return (
-      <li key={index}>
-        Nume eveniment: {event[index + 1][0]} cu locatie: {event[index + 1][1]}{" "}
-        la data de: {event[index + 1][2]}
-      </li>
+  const evenimenteViitoare = events.events.map((event, index) => {
+    let today = new Date();
+    let eventDate = event[index + 1][2];
+    let eventDateObject = new Date(
+      eventDate.split("-")[0],
+      eventDate.split("-")[1] - 1,
+      eventDate.split("-")[2]
     );
+    console.log(eventDateObject);
+    if (today <= eventDateObject)
+      return (
+        <li key={index}>
+          Nume eveniment: {event[index + 1][0]} cu locatie:{" "}
+          {event[index + 1][1]} la data de: {event[index + 1][2]}
+        </li>
+      );
+  });
+  const evenimenteTrecute = events.events.map((event, index) => {
+    let today = new Date();
+    let eventDate = event[index + 1][2];
+    let eventDateObject = new Date(
+      eventDate.split("-")[0],
+      eventDate.split("-")[1] - 1,
+      eventDate.split("-")[2]
+    );
+    console.log(eventDateObject);
+    if (today > eventDateObject)
+      return (
+        <li key={index}>
+          Nume eveniment: {event[index + 1][0]} cu locatie:{" "}
+          {event[index + 1][1]} la data de: {event[index + 1][2]}
+        </li>
+      );
   });
   return (
     <>
-      <h1>Lista evenimente</h1>
-      <ul>{listItems}</ul>
+      <h1>Lista evenimente viitoare</h1>
+      <ul>{evenimenteViitoare}</ul>
+      <h1>Lista evenimente trecute</h1>
+      <ul>{evenimenteTrecute}</ul>
     </>
   );
 }
