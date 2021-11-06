8b00eefb711049d9d62e8f116c281b055877d383
Marc Spataru <marc.spataru@gmail.com>
2021-11-06 03:42:09 +0000

    multe

--- pages/creeazaEveniment.js
+++ pages/creeazaEveniment.js
@@ -1,30 +1,94 @@
+import { useState } from "react";
+
 export default function creeazaEveniment() {
+  const [coduri, setCoduri] = useState("");
   const onSubmit = async (e) => {
     e.preventDefault();
     const numeeveniment = e.target.numeeveniment.value;
     const locatieeveniment = e.target.locatieeveniment.value;
-    const dataeveniment = e.target.dataeveniment.value;
+    const locatieridicatpuieti = e.target.locatieridicatpuieti.value;
+    const datastarteveniment = e.target.datastarteveniment.value;
+    const orastarteveniment = e.target.orastarteveniment.value;
+    const datasfarsiteveniment = e.target.datasfarsiteveniment.value;
+    const orasfarsiteveniment = e.target.orasfarsiteveniment.value;
+    const numarpuietieveniment = e.target.numarpuietieveniment.value;
     const eveniment = {
       name: numeeveniment,
       location: locatieeveniment,
-      date: dataeveniment,
+      locationsaplings: locatieridicatpuieti,
+      datestart: datastarteveniment,
+      hourstart: orastarteveniment,
+      dateend: datasfarsiteveniment,
+      hourend: orasfarsiteveniment,
+      codenr: numarpuietieveniment,
     };
     const res = await fetch("http://localhost:5000/api/create", {
       method: "POST",
       body: JSON.stringify(eveniment),
     })
       .then((res) => res.json())
-      .then((res) => console.log(res));
+      .then((res) => {
+        let tempArr = [];
+        console.log(res);
+        for (let i = 0; i < res.codes.length; i++) tempArr[i] = i;
+
+        const codulete = tempArr.map((cod, i) => {
+          return <li key={`cod${i + 1}`}>{res.codes[i]}</li>;
+        });
+        setCoduri(codulete);
+      });
   };
+
   return (
-    <form onSubmit={onSubmit}>
-      <label htmlFor="nume-eveniment">Nume eveniment</label>
-      <input type="text" name="numeeveniment" id="nume-evenimet"></input>
-      <label htmlFor="locatie-eveniment">Locatie eveniment</label>
-      <input type="text" name="locatieeveniment" id="locatie-eveniment"></input>
-      <label htmlFor="data-eveniment">Data</label>
-      <input type="date" name="dataeveniment" id="data-eveniment"></input>
-      <input type="submit"></input>
-    </form>
+    <>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="nume-eveniment">Nume eveniment</label>
+        <input type="text" name="numeeveniment" id="nume-evenimet"></input>
+        <label htmlFor="locatie-eveniment">Locatie eveniment</label>
+        <input
+          type="text"
+          name="locatieeveniment"
+          id="locatie-eveniment"
+        ></input>
+        <label htmlFor="locatie-ridicat-puieti">Locatie ridicat puieti</label>
+        <input
+          type="text"
+          name="locatieridicatpuieti"
+          id="locatie-ridicat-puieti"
+        ></input>
+        <label htmlFor="data-start-eveniment">Data start eveniment</label>
+        <input
+          type="date"
+          name="datastarteveniment"
+          id="data-start-eveniment"
+        ></input>
+        <label htmlFor="ora-start-eveniment">Ora start eveniment</label>
+        <input
+          type="time"
+          name="orastarteveniment"
+          id="ora-start-eveniment"
+        ></input>
+        <label htmlFor="data-sfarsit-eveniment">Data sfarsit eveniment</label>
+        <input
+          type="date"
+          name="datasfarsiteveniment"
+          id="data-sfarsit-eveniment"
+        ></input>
+        <label htmlFor="ora-sfarsit-eveniment">Ora sfarsit eveniment</label>
+        <input
+          type="time"
+          name="orasfarsiteveniment"
+          id="ora-sfarsit-eveniment"
+        ></input>
+        <label htmlFor="text">Numar puieti eveniment</label>
+        <input
+          type="text"
+          name="numarpuietieveniment"
+          id="numar-puieti-eveniment"
+        ></input>
+        <input type="submit"></input>
+      </form>
+      <ul>{coduri}</ul>
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,62 @@
+import { useState } from "react";
+
 export default function Events(events) {
-  const openModal = (e) => {
-    console.log(e);
+  const [numarCoduriDeIntrodus, setNumarCoduriDeIntrodus] = useState(0);
+
+  const onChangeNumarCoduriDeIntrodus = (e) => {
+    const nr = e.target.value;
+    setNumarCoduriDeIntrodus(nr);
   };
 
-  const evenimenteViitoare = events.events.map((event, index) => {
+  const eEvenimentTrecut = (eveniment) => {
     let today = new Date();
-    let eventDate = event[index + 1][2];
+    let eventDate = eveniment;
     let eventDateObject = new Date(
       eventDate.split("-")[0],
       eventDate.split("-")[1] - 1,
       eventDate.split("-")[2]
     );
-    if (today <= eventDateObject)
+    return today > eventDateObject ? true : false;
+  };
+
+  const verificaCoduri = (eventName) => async (e) => {
+    e.preventDefault();
+    let body = {
+      name: eventName,
+      codes: [],
+    };
+    for (let i = 0; i < numarCoduriDeIntrodus; i++) {
+      body.codes[i] = e.target[i].value;
+    }
+    const res = await fetch("http://localhost:5000/api/verify", {
+      method: "POST",
+      body: JSON.stringify(body),
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  };
+
+  const templateText = (eventInfo) => {
+    return (
+      <>
+        Nume eveniment: {eventInfo[0]} cu locatie: {eventInfo[1]} la data de:{" "}
+        {eventInfo[2]} ora: {eventInfo[3]}, si se sfarseste la data de:{" "}
+        {eventInfo[4]} ora {eventInfo[5]}. Se vor planta: {eventInfo[6].length}{" "}
+        puieti. Puietii vor fi ridicati de la: {eventInfo[7]}
+      </>
+    );
+  };
+
+  const evenimenteViitoare = events.events.map((event, index) => {
+    if (!eEvenimentTrecut(event[index + 1][2]))
       return (
         <li key={index}>
-          Nume eveniment: {event[index + 1][0]} cu locatie:{" "}
-          {event[index + 1][1]} la data de: {event[index + 1][2]}{" "}
+          {templateText(event[index + 1])}
           <button
             type="button"
             data-bs-toggle="modal"
             className="btn btn-primary"
             data-bs-target={`#exampleModal${index}`}
-            onClick={openModal}
           >
             Vreau sa particip
           </button>
@@ -46,17 +81,72 @@ export default function Events(events) {
                   ></button>
                 </div>
                 <div className="modal-body">...</div>
-                <div className="modal-footer">
+              </div>
+            </div>
+          </div>
+        </li>
+      );
+  });
+
+  const evenimenteTrecute = events.events.map((event, index) => {
+    let tempArr = [];
+    for (let i = 0; i < numarCoduriDeIntrodus; i++) tempArr[i] = i;
+
+    const inputElementsCoduri = tempArr.map((cod, i) => {
+      return (
+        <li key={`labelcod${i + 1}`}>
+          <label htmlFor={`cod${i + 1}`}>Cod {i + 1}</label>
+          <input type="text" name={`cod${i + 1}`} id={`cod${i + 1}`}></input>
+        </li>
+      );
+    });
+    if (eEvenimentTrecut(event[index + 1][2]))
+      return (
+        <li key={index}>
+          {templateText(event[index + 1])}
+          <button
+            type="button"
+            data-bs-toggle="modal"
+            className="btn btn-primary"
+            data-bs-target={`#exampleModal${index}`}
+          >
+            Verifica coduri
+          </button>
+          <div
+            className="modal fade"
+            id={`exampleModal${index}`}
+            tabIndex="-1"
+            aria-labelledby={`exampleModalLabel${index}`}
+            aria-hidden="true"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title" id={`exampleModalLabel${index}`}>
+                    Modal title for {event[index + 1][0]}
+                  </h5>
                   <button
                     type="button"
-                    className="btn btn-secondary"
+                    className="btn-close"
                     data-bs-dismiss="modal"
-                  >
-                    Close
-                  </button>
-                  <button type="button" className="btn btn-primary">
-                    Save changes
-                  </button>
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div className="modal-body">
+                  <form onSubmit={verificaCoduri(event[index + 1][0])}>
+                    {inputElementsCoduri}
+                    <input type="submit" value="Verifica codurile boss"></input>
+                  </form>
+                  <label htmlFor="numarCoduriDeIntrodus">
+                    Numar de coduri pe care le vreau sa le introduc
+                  </label>
+                  <input
+                    name={`exampleModalLabel${index}`}
+                    onChange={onChangeNumarCoduriDeIntrodus}
+                    type="text"
+                    value={numarCoduriDeIntrodus}
+                    id="numarCoduriDeIntrodus"
+                  ></input>
                 </div>
               </div>
             </div>
@@ -64,28 +154,12 @@ export default function Events(events) {
         </li>
       );
   });
-  const evenimenteTrecute = events.events.map((event, index) => {
-    let today = new Date();
-    let eventDate = event[index + 1][2];
-    let eventDateObject = new Date(
-      eventDate.split("-")[0],
-      eventDate.split("-")[1] - 1,
-      eventDate.split("-")[2]
-    );
-    if (today > eventDateObject)
-      return (
-        <li key={index}>
-          Nume eveniment: {event[index + 1][0]} cu locatie:{" "}
-          {event[index + 1][1]} la data de: {event[index + 1][2]}{" "}
-          <button className="btn btn-primary">Verifica coduri</button>
-        </li>
-      );
-  });
+
   return (
     <>
       <h1>Lista evenimente viitoare</h1>
       <ul>{evenimenteViitoare}</ul>
-      <h1>Lista evenimente trecute</h1>
+      <h1>Lista evenimente curente si trecute</h1>
       <ul>{evenimenteTrecute}</ul>
     </>
   );
